fb80058d9a2962d29722ae5687602f63346fa362
Tee Vang <teevang86@gmail.com>
2016-11-03 20:22:23 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,87 @@
+(function(){
+    //Remove and complete icons font awesome format
+    var removeFontIcon = '<i class="fa fa-trash-o fa-2x fill" aria-hidden="true"></i>';
+    var completeFontIcon = '<i class="fa fa-check-square-o fa-2x fill" aria-hidden="true"></i>';
+
+    //Prompt message 
+    var promptComplete = 'Task Completed';
+    var promptIncomplete = 'Uh oh! Someone checked off too early!!'
+    var promptRemove = 'Not needed huh??';
+
+    var prompt = document.createElement('p');
+    var container = document.getElementsByClassName('container')[0];
+
+    //User click on the add button
+    //If there is text inside item field, add that text to todo list
+    document.getElementById('add').addEventListener('click', function(){
+        var value = document.getElementById('item').value;
+
+        if (value) {
+            addItemTodo(value);
+            document.getElementById('item').value = '';
+        }
+    })
+
+    function removeItem() {
+        var item = this.parentNode.parentNode;
+        var parent = item.parentNode;
+        
+        parent.removeChild(item);
+
+        prompt.classList.add('prompt');
+        prompt.innerHTML = promptRemove;
+        container.appendChild(prompt);
+    }
+
+    function completeItem() {
+        var item = this.parentNode.parentNode;
+        var parent = item.parentNode;
+        var id = parent.id;
+
+        prompt.classList.add('prompt');
+        container.appendChild(prompt);
+
+        //Check if item should be add to the completed or to be re-added onto todo
+        var target = (id === 'todo') ? document.getElementById('completed'):document.getElementById('todo');
+
+        parent.removeChild(item);
+        target.insertBefore(item, target.childNodes[0]);
+
+        var promptMessage = (id === 'todo') ? prompt.innerHTML = promptComplete:prompt.innerHTML = promptIncomplete;
+        
+        
+        
+    }
+
+    //Add new items to todo list
+    function addItemTodo(text) {
+
+        var list = document.getElementById('todo');
+
+        var item = document.createElement('li');
+        item.innerText = text;
+
+        var buttons = document.createElement('div');
+        buttons.classList.add('buttons');
+
+        var remove = document.createElement('button');
+        remove.classList.add('remove');
+        remove.innerHTML = removeFontIcon;
+
+        //Add click event for removing the item
+        remove.addEventListener('click', removeItem);
+
+        var complete = document.createElement('button');
+        complete.classList.add('complete');
+        complete.innerHTML = completeFontIcon;
+
+        //Add click event for completing the item
+        complete.addEventListener('click', completeItem);
+
+        buttons.appendChild(remove);
+        buttons.appendChild(complete);
+        item.appendChild(buttons);
+
+        list.insertBefore(item, list.childNodes[0]);
+    }
+}());
